cc78dcf3d0ff9107eb75b2f1d0a5450d83a50ca1
PoisebTechHub <sadikadams23@gmail.com>
2023-10-23 21:49:07 -0700

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,14 +6,16 @@
  * @n: the number of bytes to fill the constant byte
  * Return: pointer to memory area s
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
-	{
-		for (i = 0, i < n) /* i is zero, and it is less than number to be filled */
-		(i++) /* i will increase until it reaches the number to be filled */
+	unsigned int i;
+	/* i is zero, and it is less than number to be filled */
+	for (i = 0; i < n;)
+	/* i will increase until it reaches the number to be filled */
+	i++;
 		{
-			s[i] = b; /* i will terminate when it reaches the bytes to be filled */
+			/* i will terminate when it reaches the bytes to be filled */
+			s[i] = b;
 		}
 		return (s);
-	}
+}
