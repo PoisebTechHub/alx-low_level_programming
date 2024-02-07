5d195acaa74514dacc457cb03a8613764029398c
PoisebTechHub <sadikadams23@gmail.com>
2024-02-07 00:16:20 -0800

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,5 @@
 #include "hash_tables.h"
 
-//Write a function that creates a hash table.
-//Prototype: hash_table_t *hash_table_create(unsigned long int size);
-//where size is the size of the array
-//Returns a pointer to the newly created hash table
-//If something went wrong, your function should return NULL
-//Below is the solution
-
 /**
  * hash_table_create - Creating a given hash table.
  * @size: Size of the array.
--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,5 @@
 #include "hash_tables.h"
 
-//Write a hash function implementing the djb2 algorithm.
-//Prototype: unsigned long int hash_djb2(const unsigned char *str);
-//You are allowed to copy and paste the function from this page
-//Below is the solution
-
-
 /**
  * hash_djb2 - The hash function implementing the djb2 algorithm.
  * @str: String to hash.
